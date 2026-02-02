3df4cc9e4d852d7f5144b2b1ed6f6d91220588da
Maab-MohamedTaha-k4t_nbcuni <Maab_MohamedTaha@comcast.com>
2026-02-02 15:55:48 -0800

    codebase cleanup

--- public/scripts/AssetsPath.js
+++
@@ -1,2 +0,0 @@
-export const home_open_source_press_release = "https://maabscorner.com/MoonRay_Open_Source_Launch_Press_Release.pdf"
-export const home_inital_press_release = "https://maabscorner.com/MoonRayPressRelease.pdf"
--- src/scripts/Menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-const tham = document.querySelector(".tham");
-const mobileMenu = document.getElementById("mobilemenu")
-tham.addEventListener('click', () => {
-  tham.classList.toggle('tham-active');
-  mobileMenu.classList.toggle('hidden');
-});
-});
